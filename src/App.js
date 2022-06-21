8aada5278a0ee22d83298b02cb74310c80637c23
Atharva kulkarni <atharvaanantkulkarni@gmail.com>
2022-06-21 15:02:20 +0530

    rating filter added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Button, Heading, Input, Text, useRadio } from '@chakra-ui/react';
-
+import { Button, Heading, Input, Text, useRadio, Box, VStack, Stack, HStack, InputGroup, InputLeftAddon, Table,
+	Thead,
+	Tbody,
+	Tfoot,
+	Tr,
+	Th,
+	Td,
+	TableCaption,
+	TableContainer,Badge } from '@chakra-ui/react';
 import { Grid, GridItem } from '@chakra-ui/react'
+import { ratings } from './Components/ratingColors';
+import bgimg from "./constants/im1.jpg";
 
+function getWindowDimensions() {
+	const { innerWidth: width, innerHeight: height } = window;
+	return {
+	  width,
+	  height
+	};
+  }
+  
+function useWindowDimensions() {
+	const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+	useEffect(() => {
+	  function handleResize() {
+		setWindowDimensions(getWindowDimensions());
+	  }
+  
+	  window.addEventListener('resize', handleResize);
+	  return () => window.removeEventListener('resize', handleResize);
+	}, []);
+  
+	return windowDimensions;
+}
+  
 
 function App() {
 	//constants
@@ -14,17 +46,20 @@ function App() {
 	const [userData, setUserData] = useState([]);
 	const [userProblemList, setUserProblemList] = useState([]);
 	const [intersection, setIntersection] = useState([]);
-	const [union, setUnion] = useState([]);
 	const [info, setInfo] = useState([]);
+	const [userRatingList, setUserRatingList] = useState([]);
+	const [rating, setRating] = useState(0);
 
 	//Handlers
 	function typing(event) {
 		setCurUser(event.target.value);
 	}
 
+	const handleRatingChange = (e) => {
+		setRating(e.target.value);
+	}
 
 	const getIntersection = (e) => {
-		console.log(userData.length);
 		if (userData.length >= 2) {
 			let temp = intersection;
 			for (var i = 1; i < userData.length; i++) {
@@ -35,30 +70,42 @@ function App() {
 	}
 
 	const getUnion = (e) => {
-		// if (userData.length >= 2) {
-		// 	for (var i = 1; i < userData.length; i++) {
-		// 		setUnion(userData[i].filter(item1 => union.some(item2 => item1.problem.name !== item2.problem.name)));
-		// 		console.log(userData[i].filter(item1 => union.some(item2 => item1.problem.name !== item2.problem.name)));
-		// 	}
-		// 	setInfo(union);
-		// } else {
-		// 	alert("Add more users");
-		// }
 		setInfo(userProblemList);
 	}
 
+	const getRatedProblems = () => {
+		let temp = [];
+		for (var i = 0; i < userProblemList.length; i++) {
+			if (userProblemList[i].problem.rating == rating) {
+				temp.push(userProblemList[i]);
+			}
+		}
+		console.log(temp);
+		setInfo(temp);
+	}
+
 	const getUserList = (username) => {
-		var currUserIdx = userNameList.indexOf(username);
+		let currUserIdx = 0;
+		for (var i = 0; i < userRatingList.length; i++) {
+			if (username === userRatingList[i].handle){
+				currUserIdx = i;
+			}
+		}
 		setInfo(userData[currUserIdx]);
 	}
 
 	const getUsers = async () => {
-		console.log(curUser);
 		const response = await fetch("https://codeforces.com/api/user.status?handle=" + curUser);
 		const data = await response.json();
-		
-		console.log((data.result));
-		setUserNameList([...userNameList, curUser]);
+		if(response.status !== 200) {
+			alert("Please enter a valid username");
+			setCurUser("");
+			return;
+		}
+		const responseOfUser = await fetch("https://codeforces.com/api/user.info?handles=" + curUser);
+		const dataOfUser = await responseOfUser.json();
+		setUserRatingList([...userRatingList, dataOfUser.result[0]]);
+		setUserNameList([...userNameList, dataOfUser.result.handle]);
 		var tempList = [];
 		for (var i = 0; i < data.result.length; i++) {
 			const obj = data.result[i];
@@ -66,54 +113,102 @@ function App() {
 				tempList.push({problem: obj.problem, name: obj.problem.name});
 			}
 		}
+
 		var tempList2 = [...new Map(tempList.map((item) => [item["name"], item])).values()];
+
+		tempList2.sort((a, b) => (a.problem.rating > b.problem.rating) ? 1 : -1);
 		setUserData([...userData, tempList2]);
-		console.log(tempList2);
 		setInfo(tempList2);
 		setUserProblemList([...userProblemList, ...tempList2]);
 		setCurUser("");
 	}
 
+	const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+	useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowHeight(window.innerHeight);
+        };
+        
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
 
 	useEffect(() => {
 		if (userData.length >= 1){
 			setIntersection(userData[0]);
-			setUnion(userData[0]);
 		}
 	}, [userData])
 	
-	
+	// const { width, height } = useWindowDimensions();
 	return (
-		<div>
-			<Heading>CodeForces Visualizr</Heading>
-			<Grid templateColumns='repeat(10, 1fr)' gap={2} >{ userNameList.map(item => (
-				<GridItem w='100%' key={item}>
-					<Button onClick={getUserList(item)}>
-						{item}
-					</Button>
-				</GridItem>
-			))}</Grid>
-			<Input onChange={typing}placeholder="enter username" value={curUser}></Input>
-			<Button onClick={getUsers}>Add</Button>
-			<Button onClick={getIntersection}>Intersection</Button>
-			<Button onClick={getUnion}>Union</Button>
-			<Text>{info.length}</Text>
-			{
-				info.map((item, ind) => (
-					<p key={ind}><a href={str + item.problem.contestId + "/" + item.problem.index}>{ item.problem.name + " " + item.problem.rating}</a></p>
-				))
-			}
+		<div style={ { backgroundImage : `url(${bgimg})`, height: "80000px"} }>
+		<Box bg='transparent' alignItems='center'>
+			<VStack pt={10} pb={125} px='auto' spacing={5} direction={['column', 'column', 'row']}>
+					<Heading style={{ color : "white"}}>CodeForces Visualizr</Heading>
+				<HStack>{ userRatingList.map(item => (
+						<Button colorScheme={ratings[item.rank] || "red"} onClick={() => getUserList(item.handle)}>
+							{item.handle}
+						</Button>
+				))}</HStack>
+			<HStack>
+				<InputGroup w="80%" alignItems="center">
+					<InputLeftAddon children="CF Handle" />
+					<Input style={{color : "white"}} onChange={typing} value={curUser} placeholder={"tourist"}></Input>
+				</InputGroup>
+						<Button colorScheme="white" onClick={getUsers}>Add</Button>
+					</HStack>
+					
+
+			<HStack>
+				<Button onClick={getIntersection}>Intersection</Button>
+				<Button onClick={getUnion}>Union</Button>
+			</HStack>
+					
+
+			<Stack alignItems={"center"}>
+				<InputGroup w="100%" alignItems="center">
+					<InputLeftAddon children="Rating " />
+					<Input placeholder="800" onChange={handleRatingChange} style={{color : "white"}} ></Input>
+					<Button mx={5} colorScheme="white" onClick={getRatedProblems}>Filter</Button>
+				</InputGroup>
+			</Stack>
+
+
+			<TableContainer>
+				<Table variant="simple">
+					<TableCaption>Ratings</TableCaption>
+					<Thead>
+						<Tr >
+							<Th style={{color : "white"}}>Sr. No</Th>
+							<Th style={{color : "white"}}>Index</Th>
+							<Th style={{color : "white"}}>Problem Name</Th>
+							<Th isNumeric style={{color : "white"}}>Rating</Th>
+							<Th style={{color : "white"}}>Link</Th>
+						</Tr>
+					</Thead>
+					<Tbody >
+						{info.map((item, idx) => (
+							<Tr>
+								<Th style={{color : "white"}}>{idx+1}</Th>
+								<Th style={{color : "white"}}>{item.problem.index}</Th>
+								<Th style={{color : "white"}}>{item.problem.name}</Th>
+								<Th style={{color : "white"}}>{item.problem.rating}</Th>
+								<Th ><Button onClick={() => {
+									window.open(str + item.problem.contestId + "/" + item.problem.index);
+								}}>Open</Button></Th>
+							</Tr>
+						))}
+					</Tbody>
+				</Table>
+			</TableContainer>
+			</VStack>
+		</Box>
 		</div>
   	);
 }
 export default App;
-
-
-//day2
-//display rating wise problems
-//union
-
-
-//day3
-//tag wala
 
---
+++ src/Components/ratingColors.js
@@ -0,0 +1,9 @@
+export const ratings = {
+    "newbie": "pink",
+    "pupil": "green",
+    "specialist": "cyan",
+    "expert": "blue",
+    "candidate master": "purple",
+    "master": "orange",
+    "international master" : "orange"
+}
